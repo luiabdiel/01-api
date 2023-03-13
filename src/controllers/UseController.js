d2b14573ea5e225188388e0bda6eedc8589beaa5
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-13 16:22:13 -0300

    feat: Criando o helper bodyParser

--- src/controllers/UseController.js
+++ src/controllers/UseController.js
@@ -28,24 +28,16 @@ module.exports = {
   },
 
   createUser(request, response) {
-    let body = '';
-    
-    request.on('data', (chunk) => {
-      body += chunk;
-    });
+    const { body } = request;
 
-    request.on('end', () => {
-      body = JSON.parse(body);
+    const lastUserId = users[users.length - 1].id;
+    const newUser = {
+      id: lastUserId + 1,
+      name: body.name,
+    };
 
-      const lastUserId = users[users.length - 1].id;
-      const newUser = {
-        id: lastUserId + 1,
-        name: body.name
-      };
-      
-      users.push(newUser);
-      
-      response.send(200, newUser);
-    });
+    users.push(newUser);
+
+    response.send(200, newUser);
   },
 };
